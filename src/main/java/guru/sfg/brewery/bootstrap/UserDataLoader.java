1307e5446900d0ddef6e13600a062433ead0c296
Ihor Horbonos <igorgorb@gmail.com>
2024-11-22 23:09:19 +0200

    added new authority and added them to roles

--- src/main/java/guru/sfg/brewery/bootstrap/UserDataLoader.java
+++ src/main/java/guru/sfg/brewery/bootstrap/UserDataLoader.java
@@ -39,12 +39,28 @@ public class UserDataLoader implements CommandLineRunner {
         Authority readBeer = authorityRepository.save(Authority.builder().permission("beer.read").build());
         Authority deleteBeer = authorityRepository.save(Authority.builder().permission("beer.delete").build());
 
+        // brewery auths
+        Authority createBrewery = authorityRepository.save(Authority.builder().permission("brewery.create").build());
+        Authority updateBrewery = authorityRepository.save(Authority.builder().permission("brewery.update").build());
+        Authority readBrewery = authorityRepository.save(Authority.builder().permission("brewery.read").build());
+        Authority deleteBrewery = authorityRepository.save(Authority.builder().permission("brewery.delete").build());
+
+        // Customer auths
+        Authority createCustomer = authorityRepository.save(Authority.builder().permission("customer.create").build());
+        Authority updateCustomer = authorityRepository.save(Authority.builder().permission("customer.update").build());
+        Authority readCustomer = authorityRepository.save(Authority.builder().permission("customer.read").build());
+        Authority deleteCustomer = authorityRepository.save(Authority.builder().permission("customer.delete").build());
+
         Role adminRole = roleRepository.save(Role.builder().name("ADMIN").build());
         Role customerRole = roleRepository.save(Role.builder().name("CUSTOMER").build());
         Role userRole = roleRepository.save(Role.builder().name("USER").build());
 
-        adminRole.setAuthorities(Set.of(createBeer, updateBeer, readBeer, deleteBeer));
-        customerRole.setAuthorities(Set.of(readBeer));
+        adminRole.setAuthorities(Set.of(createBeer, updateBeer, readBeer, deleteBeer,
+                createBrewery, updateBrewery, readBrewery, deleteBrewery,
+                createCustomer, updateCustomer, readCustomer, deleteCustomer));
+
+        customerRole.setAuthorities(Set.of(readBeer,readBrewery,readCustomer));
+
         userRole.setAuthorities(Set.of(readBeer));
 
         roleRepository.saveAll(Arrays.asList(adminRole, customerRole, userRole));

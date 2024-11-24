68531a84b687c6864f0c2d650568f702cae845e9
Ihor Horbonos <igorgorb@gmail.com>
2024-11-24 20:35:33 +0200

    added failing tests for next development

--- src/main/java/guru/sfg/brewery/repositories/CustomerRepository.java
+++ src/main/java/guru/sfg/brewery/repositories/CustomerRepository.java
@@ -17,7 +17,6 @@
 package guru.sfg.brewery.repositories;
 
 import guru.sfg.brewery.domain.Customer;
-import guru.sfg.brewery.domain.security.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -29,4 +28,6 @@ import java.util.UUID;
  */
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
     List<Customer> findAllByCustomerNameLike(String customerName);
+
+    Optional<Customer> findAllByCustomerName(String customerName);
 }

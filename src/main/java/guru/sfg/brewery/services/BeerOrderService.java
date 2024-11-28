b33e523c8a3b2212a8dc4574acdbf129644beee7
Ihor Horbonos <igorgorb@gmail.com>
2024-11-28 12:00:36 +0200

    use authenticating principle

--- src/main/java/guru/sfg/brewery/services/BeerOrderService.java
+++ src/main/java/guru/sfg/brewery/services/BeerOrderService.java
@@ -27,6 +27,8 @@ import java.util.UUID;
 public interface BeerOrderService {
     BeerOrderPagedList listOrders(UUID customerId, Pageable pageable);
 
+    BeerOrderPagedList listOrders(Pageable pageable);
+
     BeerOrderDto placeOrder(UUID customerId, BeerOrderDto beerOrderDto);
 
     BeerOrderDto getOrderById(UUID customerId, UUID orderId);
